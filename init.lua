7ed4d7fa517d2addd543c3b91624e291fcdd321c
Thiago-Pires <thiago.pires@ibm.com>
2026-07-24 15:06:24 -0300

    feat: layout

--- init.lua
+++ init.lua
@@ -71,11 +71,11 @@ vim.o.laststatus = 3
 function CustomStatusLine()
   local filename = vim.fn.expand("%:t")
   local extension = vim.fn.expand("%:e")
-  local icon = ""
-  
   local has_devicons, devicons = pcall(require, "nvim-web-devicons")
+  local icon = devicons.get_icon("", "vim") .. " " 
+  
   if has_devicons then
-    local file_icon = devicons.get_icon(filename, extension, { default = true })
+    local file_icon = devicons.get_icon(filename, extension, { default = false })
     if file_icon then
       icon = file_icon .. " "
     end
