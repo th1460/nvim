37a427f0464b506a95e804b09166d4ab7b078e23
Thiago-Pires <thiago.pires@ibm.com>
2026-09-16 13:34:37 -0300

    feat: open floating terminal

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -136,3 +136,28 @@ local function current_buffer_to_floating_win()
 end
 
 vim.keymap.set('n', '<leader>z', current_buffer_to_floating_win, { desc = "Move current buffer to floating window" })
+
+local function open_floating_terminal()
+    local width = math.floor(vim.o.columns * 0.8)
+    local height = math.floor(vim.o.lines * 0.8)
+
+    local row = math.floor((vim.o.lines - height) / 2)
+    local col = math.floor((vim.o.columns - width) / 2)
+
+    local buf = vim.api.nvim_create_buf(false, true)
+
+    local opts = {
+        relative = "editor",
+        width = width,
+        height = height,
+        row = row,
+        col = col,
+        style = "minimal",
+        border = "rounded"
+    }
+    vim.api.nvim_open_win(buf, true, opts)
+    vim.fn.termopen(vim.o.shell)
+    vim.cmd("startinsert")
+end
+
+vim.keymap.set('n', '<leader>t', open_floating_terminal, { desc = "Open floating terminal" })
